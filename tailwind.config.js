4017e385169be6b06b0fd22ab8cac9c3b4fec849
JULIA MENDES RODRIGUES <23111809@CFP127ALUNO.LOCAL>
2024-03-07 12:27:36 -0300

    iniciado

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      colors: {
+        'amarelo': '#F0CD13'
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
